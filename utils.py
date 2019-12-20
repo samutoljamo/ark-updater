551132dfa0e182b66544076dc52a1da8c8672990
Your Name <you@example.com>
2019-12-20 19:43:50 +0200

    Fixed execute function in python-valve package

--- utils.py
+++ utils.py
@@ -10,15 +10,15 @@ def get_num_players(address):
 
 
 def broadcast(message, address, password):
-    with RCON(address, password) as rcon:
+    with RCON(address, password, timeout=5) as rcon:
         print(rcon.authenticated, rcon.connected)
         print(rcon(f"broadcast {message}"))
 
 
 def get_version(steamcmd_path):
-    subprocess.run([steamcmd_path, ])
+    subprocess.run([steamcmd_path])
 
 if __name__ == '__main__':
     print(f"Running tests for {__file__}")
     print(get_num_players(("192.168.1.107", 27015)))
-    # broadcast("test", ("192.168.1.107", 27020), "dodo123")
+    #broadcast("test", ("192.168.1.107", 27020), "dodo123")
